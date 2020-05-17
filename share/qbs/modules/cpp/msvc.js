fdf3e1f6206c7f6a913c2de25d63d34824bfcfa5
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-05-17 17:58:47 +0200

    enable support for c++2a and c++2b on msvc

--- share/qbs/modules/cpp/msvc.js
+++ share/qbs/modules/cpp/msvc.js
@@ -120,9 +120,11 @@ function supportsExternalIncludesOption(input) {
 
 function addCxxLanguageVersionFlag(input, args) {
     var cxxVersion = Cpp.languageVersion(input.cpp.cxxLanguageVersion,
-            ["c++23", "c++20", "c++17", "c++14", "c++11", "c++98"], "C++");
+            ["c++23", "c++20", "c++2a", "c++17", "c++14", "c++11", "c++98"], "C++");
     if (!cxxVersion)
         return;
+    if (cxxVersion === "c++11" || cxxVersion === "c++98")
+        return; // /std option starts with C++14
 
     // Visual C++ 2013, Update 3 or clang-cl
     var hasStdOption = input.qbs.toolchain.includes("clang-cl")
@@ -135,9 +137,9 @@ function addCxxLanguageVersionFlag(input, args) {
         flag = "/std:c++14";
     else if (cxxVersion === "c++17" && hasCxx17Option(input))
         flag = "/std:c++17";
-    else if (cxxVersion === "c++20" && hasCxx20Option(input))
+    else if ((cxxVersion === "c++20" || cxxVersion === "c++2a") && hasCxx20Option(input))
         flag = "/std:c++20";
-    else if (cxxVersion !== "c++11" && cxxVersion !== "c++98")
+    else
         flag = "/std:c++latest";
     if (flag)
         args.push(flag);
